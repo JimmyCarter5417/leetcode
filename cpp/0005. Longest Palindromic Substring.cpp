9b8e885e982be05f8d824bf6ad40381119db0b51
wangqiang5417 <wangqiang5417@163.com>
2017-10-30 16:51:59 +0800

    Update and rename 0005. Longest Palindromic Substring to 0005. Longest Palindromic Substring.cpp

--- cpp/0005. Longest Palindromic Substring.cpp
+++ cpp/0005. Longest Palindromic Substring.cpp
@@ -1,19 +1,18 @@
-/*
-Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+// Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+// 
+// Example:
+// 
+// Input: "babad"
+// 
+// Output: "bab"
+// 
+// Note: "aba" is also a valid answer.
+// Example:
+// 
+// Input: "cbbd"
+// 
+// Output: "bb"
 
-Example:
-
-Input: "babad"
-
-Output: "bab"
-
-Note: "aba" is also a valid answer.
-Example:
-
-Input: "cbbd"
-
-Output: "bb"
-*/
 
 //dp
 class Solution {
