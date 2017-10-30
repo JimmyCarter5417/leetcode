6d5fd0fb7a96963b96ce66363c339701bd1184e9
wangqiang5417 <wangqiang5417@163.com>
2017-10-30 16:49:31 +0800

    Update and rename 0003. Longest Substring Without Repeating Characters to 0003. Longest Substring Without Repeating Characters.cpp

--- cpp/0003. Longest Substring Without Repeating Characters.cpp
+++ cpp/0003. Longest Substring Without Repeating Characters.cpp
@@ -1,14 +1,13 @@
-/*
-Given a string, find the length of the longest substring without repeating characters.
+//Given a string, find the length of the longest substring without repeating characters.
 
-Examples:
+//Examples:
 
-Given "abcabcbb", the answer is "abc", which the length is 3.
+//Given "abcabcbb", the answer is "abc", which the length is 3.
 
-Given "bbbbb", the answer is "b", with the length of 1.
+//Given "bbbbb", the answer is "b", with the length of 1.
 
-Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
-*/
+//Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
+
 
 class Solution {
 public:
