be4ae55af271079b3bd20205147129d2132d186d
wangqiang5417 <wangqiang5417@163.com>
2017-11-02 13:53:21 +0800

    Create 0695. Max Area of Island.cpp

---
+++ cpp/0695. Max Area of Island.cpp
@@ -0,0 +1,60 @@
+// Given a non-empty 2D array grid of 0's and 1's, an island is a group of 1's (representing land) connected 4-directionally (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+//
+// Find the maximum area of an island in the given 2D array. (If there is no island, the maximum area is 0.)
+//
+// Example 1:
+// [[0,0,1,0,0,0,0,1,0,0,0,0,0],
+//  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+//  [0,1,1,0,1,0,0,0,0,0,0,0,0],
+//  [0,1,0,0,1,1,0,0,1,0,1,0,0],
+//  [0,1,0,0,1,1,0,0,1,1,1,0,0],
+//  [0,0,0,0,0,0,0,0,0,0,1,0,0],
+//  [0,0,0,0,0,0,0,1,1,1,0,0,0],
+//  [0,0,0,0,0,0,0,1,1,0,0,0,0]]
+// Given the above grid, return 6. Note the answer is not 11, because the island must be connected 4-directionally.
+// Example 2:
+// [[0,0,0,0,0,0,0,0]]
+// Given the above grid, return 0.
+// Note: The length of each dimension in the given grid does not exceed 50.
+
+
+class Solution {
+public:
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        if (grid.empty() || grid.front().empty())
+            return 0;
+        
+        int res = 0;
+        vector<vector<bool>> visit(grid.size(), vector<bool>(grid.front().size(), false));
+        
+        for (int i = 0; i < grid.size(); ++i)
+        {
+            for (int j = 0; j < grid.front().size(); ++j)
+            {
+                if (grid[i][j] == 1 && !visit[i][j])
+                {
+                    int count = 0;
+                    dfs(grid, visit, i, j, count);
+                    
+                    res = max(res, count);
+                }
+            }
+        }
+        
+        return res;
+    }
+    
+    void dfs(const vector<vector<int>>& grid, vector<vector<bool>>& visit, int x, int y, int& count)
+    {
+        if (x < 0 || x >= grid.size() || y < 0 || y >= grid.front().size() || grid[x][y] == 0 || visit[x][y])
+            return;
+        
+        count++;
+        visit[x][y] = true;
+        
+        dfs(grid, visit, x - 1, y, count);
+        dfs(grid, visit, x + 1, y, count);
+        dfs(grid, visit, x, y - 1, count);
+        dfs(grid, visit, x, y + 1, count);
+    }
+};
