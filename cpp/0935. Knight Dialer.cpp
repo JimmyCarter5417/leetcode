4c7e24a208593a0d5ff37f634e4c4c4715d69bd5
wangqiang5417 <wangqiang5417@gmail.com>
2018-11-09 02:39:27 +0800

    Update 0935. Knight Dialer.cpp

--- cpp/0935. Knight Dialer.cpp
+++ cpp/0935. Knight Dialer.cpp
@@ -1,6 +1,7 @@
 // A chess knight can move as indicated in the chess diagram below:
 
-          
+<div align=center><img src="https://github.com/wangqiang5417/leetcode/blob/master/pic/knight.png"/></div>         
+<div align=center><img src="https://github.com/wangqiang5417/leetcode/blob/master/pic/keypad.png"/></div>    
 
 // This time, we place our chess knight on any numbered key of a phone pad (indicated above), and the knight makes N-1 hops.  Each hop must be from one key to another numbered key.
 
