467139240c8eab6830e70a32a2914cf7402a40bc
wangqiang5417 <wangqiang5417@gmail.com>
2019-02-12 11:52:46 +0800

    Update 0028. Implement strStr().cpp

--- cpp/0028. Implement strStr().cpp
+++ cpp/0028. Implement strStr().cpp
@@ -12,47 +12,92 @@
 // Output: -1
 
 
+// solution I: naive loop algorithm
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int haystackSize = haystack.size();
-        int needleSize = needle.size();
-        if (needleSize == 0)
-            return 0;
-            
-        // KMP
-        // build next array
-        int next[needleSize];
-        next[0] = -1;
-        int i = 0;
-        int j = -1;
-        while (i < needleSize - 1)
+    int strStr(string haystack, string needle) { 
+        if (haystack.size() < needle.size())
         {
-            if (j == -1 || needle[i] == needle[j])
-                next[++i] = ++j;
-            else
-                j = next[j];
+            return -1;
         }
         
-        // iterate
+        for (int i = 0; i <= haystack.size() - needle.size(); i++)
+        {
+            int j;
+            
+            for (j = 0; j < needle.size(); j++)
+            {
+                if (haystack[i + j] != needle[j])
+                {
+                    break;
+                }
+            }
+            
+            if (j == needle.size())
+            {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
+
+// solution II: KMP algorithm
+class Solution {
+public:
+    int strStr(string haystack, string needle) { 
+        if (haystack.size() < needle.size())
+        {
+            return -1;
+        }
+        
+        if (needle.empty())
+        {
+            return 0;
+        }
+        
+        vector<int> next(needle.size());
+        next[0] = -1; // next[0]一定是-1
+        int i = 0;
+        int j = -1;
+        
+        // 创建next数组，用needle的第i位推导第i+1位
+        while (i < needle.size() - 1)
+        {
+            if (j == -1 || needle[i] == needle[j])
+            {
+                next[++i] = ++j;
+            }
+            else
+            {
+                j = next[j];
+            }
+        }
+        
         i = 0;
         j = 0;
-        while (i < haystackSize && j < needleSize)
+        // 利用next数组遍历haystack
+        while (i < (int)haystack.size() && j < (int)needle.size()) // 此处要注意有符号无符号的隐式转换，否则会出错
         {
             if (j == -1 || haystack[i] == needle[j])
             {
-                ++i;
-                ++j;
+                i++;
+                j++;
             }
             else
             {
                 j = next[j];
             }
         }
-        
-        if (j == needleSize)
-            return i - needleSize;
-            
-        return -1;// haystack.empty()
+
+        if (j == needle.size())
+        {
+            return i - needle.size();
+        }
+        else
+        {
+            return -1;
+        }
     }
 };
