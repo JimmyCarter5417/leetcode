38bd0d8ce797c35ed5bb50359352a66e2564f80e
wangqiang5417 <wangqiang5417@gmail.com>
2019-02-18 17:07:41 +0800

    Update 0076. Minimum Window Substring.cpp

--- cpp/0076. Minimum Window Substring.cpp
+++ cpp/0076. Minimum Window Substring.cpp
@@ -25,15 +25,6 @@ public:
             dict[ch]++;
         }
         
-        // 此处有个优化方案：如果s中有许多t中不包含的字符，会导致窗口过大
-        // 可将s进行“瘦身”，只包含t中存在的字符
-        // 后续处理不变
-        /*string s1;
-        for (char ch: s)
-        {
-            s1.push_back(ch);
-        }*/
-
         int left = 0;
         int right = 0;
         int resLen = INT_MAX; // 记录最终结果的长度，初始化为最大值
@@ -77,6 +68,79 @@ public:
         }        
         
         // 注意窗口为空的情况
+        return resLen == INT_MAX ? "" : s.substr(resLeft, resLen);
+    }
+};
+
+// 此处有个优化方案：如果s中有许多t中不包含的字符，会导致窗口过大
+// 可将s进行“瘦身”，只包含t中存在的字符
+// 后续处理类似
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (s.empty() || t.empty() || s.size() < t.size())
+        {
+            return "";
+        }        
+        
+        unordered_map<char, int> dict;  
+        for (char ch: t)
+        {
+            dict[ch]++;
+        }
+        
+        // 此处多了一个瘦身数组v，仅记录t中出现的字符在s中的位置
+        vector<int> v;
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (dict.find(s[i]) != dict.end())
+            {
+                v.push_back(i);
+            }
+        }
+        
+        // 这时left和right就代表v中的位置了
+        int left = 0; 
+        int right = 0;
+        int resLen = INT_MAX;
+        int resLeft = 0;
+        int resRight = 0;
+        int required = dict.size();
+        int formed = 0;
+        unordered_map<char, int> window;
+        while (right < v.size())
+        {
+            char ch = s[v[right]]; // 一定要转成s中的位置          
+            window[ch]++;
+            
+            if (dict.find(ch) != dict.end() && dict[ch] == window[ch])
+            {
+                formed++;
+            }
+            
+            while (left <= right && formed == required)
+            {
+                // 一定要转成s中的位置         
+                if (resLen > v[right] - v[left] + 1)
+                {
+                    resLen = v[right] - v[left] + 1;
+                    resLeft = v[left];
+                    resRight = v[right];
+                }
+                
+                char ch = s[v[left]];
+                window[ch]--;
+                if (dict.find(ch) != dict.end() && window[ch] < dict[ch])
+                {
+                    formed--;
+                }
+                
+                left++;
+            }
+            
+            right++;
+        }        
+        
         return resLen == INT_MAX ? "" : s.substr(resLeft, resLen);
     }
 };
