92f84600d9f2dcba6cc27c28748c20f3d963e9a7
wangqiang5417 <wangqiang5417@gmail.com>
2019-03-09 00:15:22 +0800

    add 0201 to 0205

---
+++ cpp/0205. Isomorphic Strings.cpp
@@ -0,0 +1,44 @@
+//Given two strings s and t, determine if they are isomorphic.
+//
+//Two strings are isomorphic if the characters in s can be replaced to get t.
+//
+//All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+//
+//Example 1:
+//
+//Input: s = "egg", t = "add"
+//Output: true
+//
+//Example 2:
+//Input: s = "foo", t = "bar"
+//Output: false
+//
+//Example 3:
+//Input: s = "paper", t = "title"
+//Output: true
+//Note:
+//You may assume both s and t have the same length.
+//
+
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int n = s.size();
+        int m1[256] = {0};
+        int m2[256] = {0};
+        
+        for (int i = 0; i < n; i++)
+        {
+            if (m1[s[i]] != m2[t[i]])
+            {
+                return false;
+            }
+            
+            m1[s[i]] = m2[t[i]] = i + 1; // 记录两个对应字符出现的位置，一定是相同的
+        }
+        
+        return true;
+    }
+};
+
