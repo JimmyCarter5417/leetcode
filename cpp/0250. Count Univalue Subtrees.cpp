399a7b337a38acdb8ae961e5399774621d676e43
wangqiang5417 <wangqiang5417@gmail.com>
2019-03-18 14:14:51 +0800

    Update 0250. Count Univalue Subtrees.cpp

--- cpp/0250. Count Univalue Subtrees.cpp
+++ cpp/0250. Count Univalue Subtrees.cpp
@@ -18,15 +18,23 @@
 class Solution {
 public:
     int res = 0;
+    
     int countUnivalSubtrees(TreeNode* root) {
-        if (!root) return res;
-        if (isUnival(root, root->val)) ++res;
+        if (!root) 
+            return res;
+        if (isUnival(root, root->val)) // 这里会产生大量重复的递归
+            ++res;
+        
         countUnivalSubtrees(root->left);
         countUnivalSubtrees(root->right);
+        
         return res;
     }
+    
     bool isUnival(TreeNode *root, int val, int& res) {
-        if (!root) return true;
+        if (!root) 
+            return true;
+        
         return root->val == val && isUnival(root->left, val) && isUnival(root->right, val);
     }
 }; 
@@ -39,12 +47,21 @@ public:
         isUnival(root, -1, res);
         return res;
     }
+    
     bool isUnival(TreeNode* root, int val, int& res) {
-        if (!root) return true;
-        if (!isUnival(root->left, root->val, res) | !isUnival(root->right, root->val, res)) {
+        if (!root) 
+            return true;
+        
+        bool ret1 = isUnival(root->left, root->val, res);
+        bool ret2 = isUnival(root->right, root->val, res);
+        if (!ret1 || !ret2) // 剪枝
+        {
             return false;
         }
-        ++res;
-        return root->val == val;
+        else
+        {
+            ++res; // root节点与其左右子树数字相同，可加一
+            return root->val == val; // 还要判断root节点与其父节点
+        }
     }
 };
