aa7a3d1bace25862ba001dff42e394c4c6db3f09
wangqiang5417 <wangqiang5417@gmail.com>
2019-03-20 18:03:54 +0800

    Create 0297. Serialize and Deserialize Binary Tree.cpp

---
+++ cpp/0297. Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,81 @@
+// Serialization is the process of converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+// Design an algorithm to serialize and deserialize a binary tree. There is no restriction on how your serialization/deserialization algorithm should work. You just need to ensure that a binary tree can be serialized to a string and this string can be deserialized to the original tree structure.
+
+// Example: 
+
+// You may serialize the following tree:
+
+//     1
+//    / \
+//   2   3
+//      / \
+//     4   5
+
+// as "[1,2,3,null,null,4,5]"
+// Clarification: The above format is the same as how LeetCode serializes a binary tree. You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
+
+// Note: Do not use class member/global/static variables to store states. Your serialize and deserialize algorithms should be stateless.
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Codec {
+public:
+
+    // Encodes a tree to a single string.
+    string serialize(TreeNode* root) {
+        ostringstream oss;
+        serialize(root, oss);
+        return oss.str();
+    }
+    
+    void serialize(TreeNode* root, ostringstream& oss)
+    {
+        if (!root)
+        {
+            oss<<"# ";
+        }
+        else
+        {
+            oss<<to_string(root->val)<<" "; // 插入空格作为分隔符
+            serialize(root->left, oss);
+            serialize(root->right, oss);
+        }
+    }
+
+    // Decodes your encoded data to tree.
+    TreeNode* deserialize(string data) {
+        istringstream iss(data);
+        return deserialize(iss);
+    }
+    
+    TreeNode* deserialize(istringstream& iss)
+    {
+        TreeNode* root = nullptr;
+        string str;
+        iss>>str;
+        
+        if (str == "#")
+        {
+            return nullptr;
+        }
+        
+        root = new TreeNode(stoi(str));
+        root->left = deserialize(iss);
+        root->right = deserialize(iss);
+        
+        return root;
+    }
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
