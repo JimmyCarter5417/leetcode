b109973c3b711052ac36c0f0669282c52ebc1fe2
wangqiang5417 <wangqiang5417@gmail.com>
2019-03-24 11:29:27 +0800

    Create 01021. Best Sightseeing Pair.cpp

---
+++ cpp/01021. Best Sightseeing Pair.cpp
@@ -0,0 +1,33 @@
+// Given an array A of positive integers, A[i] represents the value of the i-th sightseeing spot, and two sightseeing spots i and j have distance j - i between them.
+
+// The score of a pair (i < j) of sightseeing spots is (A[i] + A[j] + i - j) : the sum of the values of the sightseeing spots, minus the distance between them.
+
+// Return the maximum score of a pair of sightseeing spots.
+
+// Example 1:
+
+// Input: [8,1,5,2,6]
+// Output: 11
+// Explanation: i = 0, j = 2, A[i] + A[j] + i - j = 8 + 5 + 0 - 2 = 11
+ 
+// Note:
+
+// 2 <= A.length <= 50000
+// 1 <= A[i] <= 1000
+
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& A) {
+        int res = 0;
+        int mx = A[0];
+        
+        for (int i = 1; i < A.size(); i++)
+        {
+            res = max(res, mx + A[i] - i);
+            mx = max(mx, A[i] + i); // 记录前面的临时最大值
+        }
+        
+        return res;
+    }
+};
