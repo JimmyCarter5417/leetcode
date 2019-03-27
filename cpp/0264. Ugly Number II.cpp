52bc4b38315d49ce23da96bdab8c326da6fb2247
wangqiang5417 <wangqiang5417@gmail.com>
2019-03-28 02:01:32 +0800

    add 264

--- cpp/0264. Ugly Number II.cpp
+++ cpp/0264. Ugly Number II.cpp
@@ -13,3 +13,24 @@
 // n does not exceed 1690.
 
 
+// todo
+class Solution {
+public:
+    int nthUglyNumber(int n) {
+        int i2 = 0, i3 = 0, i5 = 0;
+        vector<int> res(1, 1);
+        while (res.size() < n)
+        {
+            int m2 = 2 * res[i2];
+            int m3 = 3 * res[i3];
+            int m5 = 5 * res[i5];
+            int val = min(min(m2, m3), m5);
+            res.push_back(val);
+            if (val == m2) i2++;
+            if (val == m3) i3++;
+            if (val == m5) i5++;
+        }
+        
+        return res.back();
+    }
+};
